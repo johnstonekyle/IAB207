cf4fe794cf55f32af63c3f21d651fe1b3cf6743a
Josh Buckley <josh_buckley@iinet.net.au>
2020-06-02 22:16:09 +1000

    Added user registration, login and logout.

--- marketplace/auth.py
+++ marketplace/auth.py
@@ -1,38 +1,67 @@
-from flask import ( 
-    Blueprint, flash, render_template, request, url_for, redirect
-) 
-from werkzeug.security import generate_password_hash,check_password_hash
-#from .models import User
-from .forms import LoginForm,RegisterForm
-from flask_login import login_user, login_required,logout_user
+from flask import Blueprint, render_template, request, url_for, redirect, flash
+from .forms import LoginForm, RegisterForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
 from . import db
+from .models import User
 
 
-#create a blueprint
-bp = Blueprint('auth', __name__)
+bp = Blueprint("auth", __name__)
 
 
-# this is the hint for a login function
-# @bp.route('/login', methods=['GET', 'POST'])
-# def authenticate(): #view function
-#     print('In Login View function')
-#     login_form = LoginForm()
-#     error=None
-#     if(login_form.validate_on_submit()==True):
-#         user_name = login_form.user_name.data
-#         password = login_form.password.data
-#         u1 = User.query.filter_by(name=user_name).first()
-#         if u1 is None:
-#             error='Incorrect user name'
-#         elif not check_password_hash(u1.password_hash, password): # takes the hash and password
-#             error='Incorrect password'
-#         if error is None:
-#             login_user(u1)
-#             nextp = request.args.get('next') #this gives the url from where the login page was accessed
-#             print(nextp)
-#             if next is None or not nextp.startswith('/'):
-#                 return redirect(url_for('index'))
-#             return redirect(nextp)
-#         else:
-#             flash(error)
-#     return render_template('user.html', form=login_form, heading='Login')
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    error = None
+    if(login_form.validate_on_submit()):
+        user_name = login_form.user_name.data
+        password = login_form.password.data
+        u1 = User.query.filter_by(username=user_name).first()
+
+        if u1 is None:
+            error="Incorrect username"
+        elif not check_password_hash(u1.password_hash, password):
+            error = "Incorrect password"
+        if error is None:
+            login_user(u1)
+            return redirect(url_for("main.index"))
+        else:
+            flash(error)
+
+
+    return render_template("user.html", form=login_form, heading="Login")
+
+
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    register = RegisterForm()
+    if (register.validate_on_submit()):
+        #get username, password and email from the form
+        uname = register.user_name.data
+        pwd = register.password.data
+        email = register.email_id.data
+        pnum = register.phone.data
+        status = register.seller.data
+
+        u1 = User.query.filter_by(username=uname).first()
+        if u1:
+            flash("Username already exists! Please login.")
+            return redirect(url_for("auth.login"))
+        
+        pwd_hash = generate_password_hash(pwd)
+        new_user = User(username=uname, password_hash=pwd_hash, email=email, phone=pnum, seller=status)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("main.index"))
+    
+    else:
+        return render_template("user.html", form=register, heading="Register")
+
+
+
+@bp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- marketplace/forms.py
+++ marketplace/forms.py
@@ -1,27 +1,32 @@
 
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, IntegerField, BooleanField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 
 #creates the login information
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    user_name=StringField("User Name", validators=[InputRequired('Enter user name.')])
+    password=PasswordField("Password", validators=[InputRequired('Enter user password.')])
     submit = SubmitField("Login")
 
  # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    
-    #add buyer/seller - check if it is a buyer or seller hint : Use RequiredIf field
+    user_name = StringField("User Name", validators=[InputRequired()])
+    email_id = StringField("Email Address", validators=[Email("Enter a valid email.")])
 
+    # ----- will need to change how this works as it does not allow for spaces (e.g. 0412 345 678) -----
+    # ----- also currently has no control over min and max character inputs (e.g. phone number of 123) -----
+    phone = IntegerField("Phone Number", validators=[InputRequired("Enter a phone number.")])
 
     #linking two fields - password should be equal to data entered in confirm
     password=PasswordField("Password", validators=[InputRequired(),
                   EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
 
+    #add buyer/seller - check if it is a buyer or seller hint : Use RequiredIf field
+    # ----- NB: RequiredIf has not been implemented here yet. -----
+    seller = BooleanField("Do you wish to sell products?")
+    
     #submit button
     submit = SubmitField("Register")
